2507e61d298a62a1de215e3ce4028939024a2df8
Philip Graham Willoughby <willerz+github@gmail.com>
2012-06-05 10:48:23 +0100

    First version of tools

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ snique-tools
 ============
 
 Tools for snique
+
+## snique-conceal
+
+Tool to create the hex sequence to conceal on a webpage.
+
+### Usage
+
+Edit the hardcoded key to the shared secret you are using.
+Run:
+	snique-conceal Your message goes here > file
+The file will contain the hex sequence you need to conceal.
+
+## snique-pic.php
+
+Server script to deliver concealed messages. Do not use this script as-is for your covert communication because it is hardly subtle.
+
+### Usage
+
+Upload it to a server that can run PHP.
+Put the hex you want to send out in a file named .message in the same directory.
+Link to the images from a webpage, using the full URL of the script (e.g. if your site is at http://blog.nomzit.com – it isn't because that's my site, but you get the idea):
+
+	<img src='http://blog.nomzit.com/snique-pic.php?index=0'/><img src='http://blog.nomzit.com/snique-pic.php?index=1'/>…
---
+++ snique-pic.php
@@ -0,0 +1,43 @@
+<?php
+header_remove('x-powered-by');
+header_remove('content-location');
+
+$message = file_get_contents('./.message');
+$etags = str_split($message, 32);
+$num = $_GET["index"];
+$etag = $etags[intval($num)];
+$etag = str_replace("\n",'',$etag);
+srand(intval($num));
+$chars = 'abcdef0123456789';
+while (strlen($etag) < 32)
+{
+  $etag .= $chars[rand(0, 15)];
+}
+$if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ?
+	 stripslashes($_SERVER['HTTP_IF_NONE_MATCH']) : 
+		 false ;
+
+if( false !== $if_none_match )
+{
+  $tags = split( ", ", $if_none_match ) ;
+  foreach( $tags as $tag )
+  {
+    if( $tag == $etag )
+    {
+      header( "HTTP/1.1 304 NOT MODIFIED" );
+      exit;
+    }
+  }
+}
+header( "HTTP/1.1 200 OK" );
+header( "Content-Type: image/png" );
+header( "Cache-Control: public, max-age=60, no-transform, must-revalidate" );
+header( 'Etag: "'.$etag.'"' );
+$im = imagecreate(40,40);
+imagecolorallocate($im, 255, 255, 255);
+$black = imagecolorallocate($im, 0, 0, 0);
+imagestring($im, 5, 0, 0, $num, $black);
+imagepng($im);
+imagedestroy($im);
+exit;
+?>
